1afa802c021bdc57064e9e40dd1529f7fa020dc4
Jun <me@noraesae.net>
2016-10-17 22:48:08 +0900

    Add option parser

--- src/Option.hs
+++ src/Option.hs
@@ -2,5 +2,24 @@ module Option
   ( config
   ) where
 
+import Options.Applicative hiding (option)
+
+data Option = Option { config' :: FilePath
+                     }
+
+parser :: Parser Option
+parser = Option
+         <$> strOption ( long "config"
+                      <> short 'c'
+                      <> metavar "FILE"
+                      <> help "Config file"
+                       )
+
+option :: IO Option
+option = execParser $
+  info (helper <*> parser) ( fullDesc
+                          <> header "love-arrow-shoot - A super simple reverse proxy on Warp"
+                           )
+
 config :: IO FilePath
-config = return "./config.sample.yaml"
+config = config' <$> option
