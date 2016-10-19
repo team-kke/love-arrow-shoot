e38dcb7da5e544dfbccddae851405fbe7e7791d0
Hyunje Jun <me@noraesae.net>
2016-10-19 19:16:27 +0900

    Add very basic routes
    Using wai-routing.

--- src/Server.hs
+++ src/Server.hs
@@ -4,7 +4,18 @@ module Server where
 
 import Network.HTTP.Types
 import Network.Wai
+import Network.Wai.Predicate
+import Network.Wai.Routing
+import qualified Data.ByteString.Lazy as B
+
+routes :: Routes a IO ()
+routes = do
+  get "/" (continue hello) $ constant "ya"
+  get "/:name" (continue hello) $ capture "name"
+
+hello :: B.ByteString -> IO Response
+hello name = return $
+  responseLBS status200 [] $ B.concat ["hello, ", name]
 
 server :: Application
-server req respond =
-  respond $ responseLBS status200 [] "Hello World"
+server = route $ prepare routes
