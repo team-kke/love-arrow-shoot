9dfe9f346c7fb34d2b1b74b9cfdaf3e9e98a0342
Jun <me@noraesae.net>
2016-10-21 01:09:36 +0900

    Add basic auth for __setting__

--- src/Config.hs
+++ src/Config.hs
@@ -2,19 +2,28 @@
 
 module Config
   ( getPort
+  , getAuth
   ) where
 
+import Control.Arrow ((***))
+import Control.Monad (liftM2)
+import Data.ByteString (ByteString)
+import Data.ByteString.Char8 (pack)
 import Data.Yaml
 import Network.Wai.Handler.Warp (Port)
 import System.Exit (exitFailure)
 import qualified Option
 
 data Config = Config { port :: Port
+                     , username :: String
+                     , password :: String
                      }
 
 instance FromJSON Config where
   parseJSON (Object v) = Config <$>
-                         v .: "port"
+                         v .: "port" <*>
+                         v .: "username" <*>
+                         v .: "password"
 
 config :: IO Config
 config = do
@@ -28,3 +37,6 @@ config = do
 
 getPort :: IO Port
 getPort = port <$> config
+
+getAuth :: IO (ByteString, ByteString)
+getAuth = (pack *** pack) <$> liftM2 (,) (username <$> config) (password <$> config)
--- src/Server.hs
+++ src/Server.hs
@@ -2,10 +2,12 @@
 
 module Server where
 
+import Config (getAuth)
 import Data.Maybe (fromJust)
 import Network.HTTP.Types
 import Network.Wai
 import Network.Wai.Application.Static
+import Network.Wai.Middleware.HttpAuth
 import Option (static)
 import WaiAppStatic.Types (toPieces)
 import qualified Data.ByteString.Lazy as B
@@ -16,9 +18,16 @@ staticHandler dir = staticApp
                               , ssIndices = fromJust $ toPieces ["index.html"]
                               }
 
+auth :: Middleware
+auth = basicAuth checkCredentials "My Realm"
+  where
+    checkCredentials u p = do
+      (username, password) <- getAuth
+      return $ u == username && p == password
+
 server :: Application
 server req f =
   case pathInfo req of
     "__setting__" : sub -> do
       dir <- static
-      staticHandler dir req { pathInfo = sub } f
+      auth (staticHandler dir) req { pathInfo = sub } f
