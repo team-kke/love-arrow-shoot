66cd49871037bcdca3b08ff23c7e6e7687d40c7e
Jun <me@noraesae.net>
2016-10-23 12:09:38 +0900

    Implement simple counter
    Use purescript 0.9 and install with local npm.

--- purs/Main.purs
+++ purs/Main.purs
@@ -1,9 +1,49 @@
 module Main where
 
 import Prelude
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "Hello sailor!"
+import Control.Monad.Aff (Aff, later')
+import Control.Monad.Eff (Eff)
+
+import Halogen as H
+import Halogen.HTML.Indexed as HH
+import Halogen.HTML.Properties.Indexed as HP
+import Halogen.Util (runHalogenAff, awaitBody)
+
+type State = Int
+
+initialState :: State
+initialState = 0
+
+data Query a = Tick a
+
+ui :: forall g. H.Component State Query g
+ui = H.component { render, eval }
+  where
+
+  render :: State -> H.ComponentHTML Query
+  render n =
+    HH.div_
+      [ HH.h1
+          [ HP.id_ "header" ]
+          [ HH.text "counter" ]
+      , HH.p_
+          [ HH.text (show n) ]
+      ]
+
+  eval :: Query ~> H.ComponentDSL State Query g
+  eval (Tick next) = do
+    H.modify (_ + 1)
+    pure next
+
+-- | Run the app
+main :: Eff (H.HalogenEffects ()) Unit
+main = runHalogenAff do
+  body <- awaitBody
+  driver <- H.runUI ui initialState body
+  setInterval 1000 $ driver (H.action Tick)
+
+setInterval :: forall e a. Int -> Aff e a -> Aff e Unit
+setInterval ms a = later' ms $ do
+  a
+  setInterval ms a
