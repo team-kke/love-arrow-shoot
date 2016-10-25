fbb53d52c90dba617fef5ea7484db41962a1d512
Hyunje Jun <me@noraesae.net>
2016-10-25 10:53:15 +0900

    Implement proxy server according to proxy settings

--- src/Server/Proxy.hs
+++ src/Server/Proxy.hs
@@ -4,12 +4,20 @@ module Server.Proxy
   ( proxyServer
   ) where
 
+import Database
 import Network.HTTP.Client (newManager, defaultManagerSettings)
 import Network.HTTP.ReverseProxy
+import Network.HTTP.Types.Status (status502)
 import Network.Wai
+import Text.Regex.Posix ((=~))
 
 proxyRules :: Request -> IO WaiProxyResponse
-proxyRules _ = return $ WPRProxyDest $ ProxyDest "127.0.0.1" 8000 -- FIXME
+proxyRules req = do
+  let path = rawPathInfo req
+  rules <- map snd <$> getProxies
+  case filter ((path =~) . pathPattern) rules of
+    Proxy _ host port : _ -> return $ WPRProxyDest $ ProxyDest host port
+    _ -> return $ WPRResponse $ responseLBS status502 [] "nah"
 
 proxyServer :: Application
 proxyServer req f = do
