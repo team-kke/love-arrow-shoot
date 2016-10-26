ed7adfdd1eb8707c7ff8c54d9c05a298e9333e0a
Jun <me@noraesae.net>
2016-10-26 22:43:36 +0900

    Add IsForeign and Show instance to Proxy

--- purs/Proxy.purs
+++ purs/Proxy.purs
@@ -4,7 +4,20 @@ module Proxy
 
 import Prelude
 
+import Data.Generic (class Generic)
+import Data.Foreign.Class (class IsForeign)
+import Data.Foreign.Generic (readGeneric, defaultOptions)
+
 data Proxy = Proxy { pathPattern :: String
                    , proxyHost :: String
                    , proxyPort :: Int
                    }
+
+derive instance genericProxy :: Generic Proxy
+
+instance showProxy :: Show Proxy where
+  show (Proxy {pathPattern, proxyHost, proxyPort}) =
+    "(Proxy " <> pathPattern <> " -> " <> proxyHost <> ":" <> show proxyPort
+
+instance proxyIsForeign :: IsForeign Proxy where
+  read = readGeneric defaultOptions
